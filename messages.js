70427d0126f51768a44b4bdb8d23d104ab7f7fd6
theandrewfat <the.andrew.fat@gmail.com>
2026-03-08 19:50:49 +0100

    Add files via upload

--- messages.js
+++ messages.js
@@ -1,19 +1,19 @@
 // Agrega aquí todos los mensajes que quieras, uno por línea
 window.BUBBLE_MESSAGES = [
-  '¡Hola mamá!',
-  '¡Hola papá!',
+  '¡Hola mamá y papá!',
   '¡Hola yayos!',
   'Un momento... ¡¡¿TENGO BARBA?!!',
   '¡Hola abuelos!',
   '¡Hola tía María!',
   '¡Hola tía María José!',
-  '¡Hola prima Sara! sorry, soy chico jiji',
+  'Tu eres...¡la famosa prima Sara! Holii',
   '¡Hola tío David!',
   'Pues sí... tengo barba... qué raro...',
   '¡Hola primo Daniel!, soy el nuevo Marqués',
-  '¡Hola primos de papá y mamá! ¡¡sois un montón!!',
+  '¡Hola primos de papá y mamá!',
   '¡Hola prima Ainhoa!',
   '¡Hola primo Mario!',
   '¡Hola prima Ana!',
-  '¡Hola amigos de papá y mamá! ¡¡Nos vemos en los bares!!🍻🍻'
+  '¡Hola amigos de papá y mamá!🍻🍻',
+
 ];
